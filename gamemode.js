9a3a8d8057269e296e5cfcf114a7d05827b90fe6
GeorgeOL <george6472@gmail.com>
2023-03-25 11:23:37 +0200

    Advanced

--- gamemode.js
+++ gamemode.js
@@ -20,3 +20,26 @@ class Mage extends Player{
 
 const Mage1 = new Mage(`Ion`, `fire`);
 const Mage2 = new Mage(`Andra`, `healer`);
+
+class Animal{
+    constructor(name, type, color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+    introduce(){
+        console.log(`mooo i am a ${this.type}, my name is ${this.name}, I a ${this.type}.`)
+    }
+}
+
+class Mamal extends Animal{
+    constructor(name, type, color) {
+        super(name, type, color);
+    }
+    moo (){
+        console.log(` mooo ${this.type}, ${this.name}, ${this.color}`);
+    }
+}
+
+const cow1 = new Mamal(`Virginica` , `laptareasa`, `maro`);
+const cow2 = new Mamal(`andra`, `carne`, `alb`);
